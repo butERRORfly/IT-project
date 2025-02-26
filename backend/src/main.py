08ef31ae41df37c51af8fcb350721f976981bfde
butERRORfly <progamer_228@inbox.ru>
2025-02-26 21:52:45 +0300

    Added a base template and static files

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from backend.src.adapters.http.routers.routers import api_router
 from backend.src.config.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 app.include_router(api_router)
+app.mount("/frontend/src/static", StaticFiles(directory="frontend/src/static"), name="static")
+
+templates = Jinja2Templates(directory="frontend/src/templates")
