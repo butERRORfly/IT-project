9ff8b57182b3000d11a892a9df9337c345167518
raketchik <arrdin32@gmail.com>
2025-04-01 21:02:33 +0300

    Update routers.py

--- src/adapters/entrypoints/api/routers.py
+++ src/adapters/entrypoints/api/routers.py
@@ -5,3 +5,4 @@ from src.adapters.entrypoints.api.v1.users import users_router
 api_router = APIRouter()
 api_router.include_router(index.router, tags=["Домашняя страница"])
 api_router.include_router(users_router.router, prefix='/auth', tags=["Авторизация"])
+api_router.include_router(icao.rout)
