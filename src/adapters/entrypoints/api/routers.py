69d92a63837705e825526b1b2a03213167343e32
butERRORfly <progamer_228@inbox.ru>
2025-04-06 20:03:04 +0300

    Updated structure and auth

--- src/adapters/entrypoints/api/routers.py
+++ src/adapters/entrypoints/api/routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from src.adapters.entrypoints.api.v1 import index
+from src.adapters.entrypoints.api.v1.index import index_router
+from src.adapters.entrypoints.api.v1.auth import auth_router
 from src.adapters.entrypoints.api.v1.users import users_router
 
 api_router = APIRouter()
-api_router.include_router(index.router, tags=["Домашняя страница"])
-api_router.include_router(users_router.router, prefix='/auth', tags=["Авторизация"])
+api_router.include_router(index_router.router, tags=["Домашняя страница"])
+api_router.include_router(auth_router.router, prefix='/auth', tags=["Аутентификация"])
+api_router.include_router(users_router.router, prefix='/users', tags=["Пользователи"])
--- src/adapters/entrypoints/api/v1/users/users_router.py
+++ src/adapters/entrypoints/api/v1/users/users_router.py
@@ -5,61 +5,16 @@ from src.adapters.entrypoints.webapps.auth import get_password_hash, create_acce
 from src.infrastructure.db.dao.users import UsersDAO
 from src.infrastructure.db.database import User
 from src.domain.schemas.users import UserRegister, UserAuth, UserChangeRole
-from src.adapters.entrypoints.webapps.dependensies import get_current_user, get_current_admin_user
+from src.adapters.entrypoints.webapps.dependencies import get_current_user, get_current_admin_user
+
 
 router = APIRouter()
 templates = Jinja2Templates(directory="src/adapters/entrypoints/templates")
 
 
-@router.post("/register/", summary="Регистрация")
-async def register_user(user_data: UserRegister) -> dict:
-    user = await UsersDAO.find_one_or_none(email=user_data.email)
-    if user:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail='Пользователь уже существует'
-        )
-    user_dict = user_data.model_dump()
-    user_dict['password'] = get_password_hash(user_data.password)
-    await UsersDAO.add(**user_dict)
-    return {'message': 'Вы успешно зарегистрированы!'}
-
-
-@router.get("/register/", response_class=HTMLResponse, summary="Страница регистрации")
-async def register_page(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
-
-
-@router.post("/login/", summary="Вход в систему")
-async def auth_user(response: Response, user_data: UserAuth):
-    check = await authenticate_user(email=user_data.email, password=user_data.password)
-    if check is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Неверная почта или пароль')
-    access_token = create_access_token({"sub": str(check.id)})
-    response.set_cookie(key="users_access_token", value=access_token, httponly=True)
-    return {'access_token': access_token, 'refresh_token': None}
-
-
-@router.get("/login/", response_class=HTMLResponse, summary="Страница вход в систему")
-async def login_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
-
-
 @router.get("/profile/", response_class=HTMLResponse, summary="Профиль пользователя")
 async def profile_page(request: Request, user: User = Depends(get_current_user)):
     return templates.TemplateResponse("profile.html", {"request": request, "user": user})
-
-
-@router.post("/logout/", summary="Выход")
-async def logout_user(response: Response):
-    redirect_response = RedirectResponse(
-        url="/auth/login/",
-        status_code=status.HTTP_303_SEE_OTHER
-    )
-    redirect_response.delete_cookie(key="users_access_token")
-    # response.set_cookie(key="flash_message", value="Вы успешно вышли", max_age=5)
-    return redirect_response
 
 
 @router.get("/all_users/", summary="Список пользователей")
