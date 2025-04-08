1394fdbe4a8cba6ecd7d330f03fac0ee5d349cb7
butERRORfly <progamer_228@inbox.ru>
2025-04-08 21:40:42 +0300

    Updated admin-panel

--- src/adapters/entrypoints/api/v1/users/users_router.py
+++ src/adapters/entrypoints/api/v1/users/users_router.py
@@ -17,9 +17,26 @@ async def profile_page(request: Request, user: User = Depends(get_current_user))
     return templates.TemplateResponse("profile.html", {"request": request, "user": user})
 
 
-@router.get("/", summary="Получить всех пользователей")
-async def get_all_users(user_data: User = Depends(get_current_admin_user)):
-    return await UsersDAO.find_all()
+# @router.get("/", summary="Получить всех пользователей")
+# async def get_all_users(user_data: User = Depends(get_current_admin_user)):
+#     return await UsersDAO.find_all()
+
+
+@router.get("/", response_class=HTMLResponse, summary="Получить всех пользователей")
+async def get_all_users_html(
+        request: Request,
+        admin_user: User = Depends(get_current_admin_user)
+):
+    users = await UsersDAO.find_all()
+
+    return templates.TemplateResponse(
+        "all_users.html",
+        {
+            "request": request,
+            "users": users,
+            "admin": admin_user
+        }
+    )
 
 
 @router.get("/{id_user}/", summary="Получить пользователя по id")
@@ -31,7 +48,6 @@ async def get_user_by_id(id_user: int, user_data: User = Depends(get_current_adm
             detail="Пользователь с указанным id не найден"
         )
     return user
-
 
 # @router.post("/set_role/", summary="Выдать роль")
 # async def set_admin_role(
